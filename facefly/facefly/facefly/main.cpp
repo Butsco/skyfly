697180b500adbfc309d0b979349621076197f52a
Bert Wijnants <bert.wijnants@gmail.com>
2015-12-05 21:42:14 +0100

    crop faces

--- facefly/facefly/facefly/main.cpp
+++ facefly/facefly/facefly/main.cpp
@@ -17,10 +17,11 @@
 using namespace std;
 using namespace cv;
 
-const char* cascadeFile = "/Users/bert/Projects/opencv-3.0.0/data/haarcascades/haarcascade_frontalface_default.xml";
+const char* cascadeFile = "/Users/bert/Projects/opencv-3.0.0/data/haarcascades/haarcascade_frontalface_alt2.xml";
 
 const char* frameDir = "/Users/bert/Projects/skyfly/facefly/s01e01/frames/";
 const char* outDir = "/Users/bert/Projects/skyfly/facefly/s01e01/output/";
+const char* faceDir = "/Users/bert/Projects/skyfly/facefly/s01e01/faces/";
 
 
 CascadeClassifier faceCascade;
@@ -28,13 +29,14 @@ CascadeClassifier faceCascade;
 Mat src;
 Mat grey;
 Mat output;
+Mat face;
 
 int main(int argc, char** argv)
 {
     cout << "Init\n";
     faceCascade = CascadeClassifier(cascadeFile);
     
-    for (int i=1; i<=3017; i++) {
+    for (int i=1; i<=1688; i++) {
         processFrame(i, false);
     }
     
@@ -44,6 +46,7 @@ int main(int argc, char** argv)
 void processFrame(int frameId, bool display) {
     char framePath[150];
     char outPath[150];
+    char facePath[150];
     sprintf(framePath, "%s%d.png", frameDir, frameId);
     sprintf(outPath, "%s%d.png", outDir, frameId);
     
@@ -54,7 +57,7 @@ void processFrame(int frameId, bool display) {
     
     vector<Rect> faces;
     
-    faceCascade.detectMultiScale(grey, faces, 1.1, 5, CV_HAAR_SCALE_IMAGE, Size(30, 30));
+    faceCascade.detectMultiScale(grey, faces, 1.1, 5, CV_HAAR_SCALE_IMAGE, Size(50, 50));
     
     printf("frame %d: %zd faces.\n", frameId, faces.size());
     
@@ -71,6 +74,12 @@ void processFrame(int frameId, bool display) {
                       CV_RGB(255,0,0),
                       2
                       );
+            
+            // crop immage
+            face = src(r);
+            sprintf(facePath, "%s%d_%d.png", faceDir, frameId, i);
+            imwrite(facePath, face);
+        
         }
         
         imwrite(outPath, output);
--- facefly/frames.sh
+++ facefly/frames.sh
@@ -1,1 +1,1 @@
-./ffmpeg -i s01e01/video/Game.of.Thrones.S01E09.Baelor.HDTV.XviD-FQM.avi -vf fps=2 s01e01/frames/%d.png
+./ffmpeg -i s01e01/video/Game.of.Thrones.S01E09.Baelor.HDTV.XviD-FQM.avi -vf fps=0.5 s01e01/frames/%d.png
