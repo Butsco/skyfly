b7ca9324d7f072b1134d0a681db1e3fd926e3730
Bert Wijnants <bert.wijnants@gmail.com>
2015-12-05 18:08:59 +0100

    open window with image and close on key

--- facefly/facefly/facefly/main.cpp
+++ facefly/facefly/facefly/main.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #import <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <unistd.h>
 #include <stdio.h>
 #include <errno.h>
@@ -19,7 +20,7 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
-    std::cout << "Init\n";
+    cout << "Init\n";
     
     stringstream ssIn;
     string framesDir = string("/Users/bert/Projects/skyfly/facefly/s01e01/frames/");
@@ -27,40 +28,23 @@ int main(int argc, char** argv)
     ssIn << framesDir << frameFilename;
     string framePath = ssIn.str();
     
-    std::cout << "framepath: " << framePath << "\n";
+    cout << "frame: " << frameFilename << "\n";
+    //cout << "framepath: " << framePath << "\n";
     
     stringstream ssOut;
     string outDir = string("/Users/bert/Projects/skyfly/facefly/s01e01/output/");
     ssOut << outDir << frameFilename;
     string outPath = ssOut.str();
     
-    std::cout << "outPath: " << outPath << "\n";
+    //cout << "outPath: " << outPath << "\n";
     
     Mat src = imread(framePath, 1 );
     
+    imwrite(outPath, src);
+    
     namedWindow(frameFilename, WINDOW_AUTOSIZE );
     imshow("Unprocessed Image",src);
-    
-    
-    IplImage * pInpImg = 0;
-    
-    // Load an image from file - change this based on your image name
-    pInpImg = cvLoadImage(framePath.c_str(), CV_LOAD_IMAGE_UNCHANGED);
-    if(!pInpImg)
-    {
-        fprintf(stderr, "failed to load input image\n");
-        return -1;
-    }
-    
-    // Write the image to a file with a different name,
-    // using a different image format -- .png instead of .jpg
-    if( !cvSaveImage(outPath.c_str(), pInpImg) )
-    {
-        fprintf(stderr, "failed to write image file\n");
-    }
-    
-    // Remember to free image memory after using it!
-    cvReleaseImage(&pInpImg);
+    waitKey(0);
     
     return 0;
 }
